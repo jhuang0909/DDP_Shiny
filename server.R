6869862b3e3f450a2cc1482e248899af2e471d47
jhuang0909 <jhuang@osisoft.com>
2017-04-16 16:50:55 -0400

    Add validation

--- server.R
+++ server.R
@@ -2,35 +2,57 @@ library(shiny)
 library(plotly)
 library(zoo)
 
+presidents <- get("presidents", "package:datasets")
+presidents.df <- data.frame(rating=as.matrix(presidents), date=as.Date(time(presidents)))
+getSubset <- function(minY, maxY) {
+                        # Need to cover at least two valid ratings
+                        subset <- presidents.df %>% filter(date >= minY & date <= maxY & !is.na(rating))
+                        return(subset)
+                }                
+
 shinyServer(function(input, output) {
-                presidents.df <- data.frame(rating=as.matrix(presidents), date=as.Date(time(presidents)))
-                output$plot1 <- renderPlotly({
+                minY <- min(presidents.df$date)
+                maxY <- max(presidents.df$date)
+
+                dfSubset <- reactive({
                         minY <- input$sliderYear[1]
                         maxY <- input$sliderYear[2]
-                        presidents.df.subset <- presidents.df %>% 
-                                filter(date >= minY & date <= maxY)
+                        subset <- getSubset(minY, maxY)
+                        validate(
+                                need(nrow(subset) > 0, "Please select a valid time range")
+                        )
+                        return(subset)
+                })
+                
+                output$plot1 <- renderPlotly({
+                        presidents.df.subset <- dfSubset()
+                        
                         minRatings <- presidents.df.subset[which(presidents.df.subset$rating == 
                                         min(presidents.df.subset$rating, na.rm = TRUE)),]
                         maxRatings <- presidents.df.subset[which(presidents.df.subset$rating == 
                                         max(presidents.df.subset$rating, na.rm = TRUE)),]
-                        xlab <- ifelse(input$show_xlab, "Year", "")
+                        xlab <- "Year"
                         ylab <- ifelse(input$show_ylab, "Approval Ratings", "")
                         plot_ly(data = presidents.df.subset, x = ~date, showlegend = FALSE) %>% 
                                 layout(yaxis = list(title = ylab), 
                                        xaxis = list(title = xlab)) %>%
-                                
                                 add_lines(y = ~rating, 
+                                          name = "",
                                           line = list(color = "#00526d", width = 4)) %>%
                                 add_markers(x = minRatings$date,
                                             y = minRatings$rating,
+                                            name = "Minimum Rating",
                                             marker = list(size = 15, color = "#f44141")) %>%
                                 add_markers(x = maxRatings$date,
                                             y = maxRatings$rating,
+                                            name = "Maximum Rating",
                                             marker = list(size = 15, color = "#42f445"))
                 })
         
-        output$aveRating <- renderText({ 
-                subset <- presidents.df %>% filter(date >= input$sliderYear[1] & date <= input$sliderYear[2])
-                paste0("Average rating is ", sprintf("%.2f",mean(subset$rating, na.rm = TRUE)))
+                output$aveRating <- renderText({ 
+                        if(input$show_aveRating) {
+                                presidents.df.subset <- dfSubset()
+                                paste0("Average Rating = ", sprintf("%.2f",mean(presidents.df.subset$rating, na.rm = TRUE)))
+                        }
         })
 })
--- ui.R
+++ ui.R
@@ -2,13 +2,25 @@ library(shiny)
 library(plotly)
 
 shinyUI(fluidPage(
+        
+        tags$head(
+                tags$style(HTML("
+                      .shiny-output-error-validation {
+                        color: green;
+                      }
+                    "))
+        ),
+        
         titlePanel("Plot Quarterly Approval Ratings of US Presidents"),
+        
+        fluidRow("This Shiny application demonstrates the approval ratings of US predisents between 1945 and 1975.  The raw data is from the R Datasets Package.  Using the slidebar, you may choose any date range between 1945 and 1975.  The Shiny server will automatically show the rating trace for the given range in a Plotly plot, the min and max are also marked.  The average rating may be shown in a text box based on whether Show/Hide checkbox is checked."),
+        
         sidebarLayout(
                 sidebarPanel(
+                        maxRange <- c(as.Date("1945-01-01"), as.Date("1974-10-01")),
                         sliderInput("sliderYear", "Pick Minimum and Maximum Dates",
-                                    as.Date("1945-01-01"), as.Date("1975-12-31"), value = c(as.Date("1945-01-01"), as.Date("1975-12-31")), timeFormat = "%Y", step = 30),
-                        checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
-                        checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
+                                    maxRange[1], maxRange[2], value = maxRange, timeFormat = "%Y", step = 100),
+                        checkboxInput("show_aveRating", "Show/Hide Average Rating", value = TRUE),
                         textOutput("aveRating")
                         ),
                 mainPanel(
